f9698289dfc4956c135e776ef19ee14175614ece
2no <kazunori.ninomiya@gmail.com>
2016-07-04 17:07:17 +0900

    rebuilding site 2016年  7月  4日 月曜日 17:07:17 JST

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin source
-git subtree push --prefix=public origin gh-pages
+git subtree split --prefix public -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
